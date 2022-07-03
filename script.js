650a01ca889419f9a5d1c40b1858216d1f4d8dda
Suryakant Tripathi <77002129+Tsurya06@users.noreply.github.com>
2022-07-04 03:54:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-fetch('https://api.chucknorris.io/jokes/random?category=science')
+fetch('https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Spooky,Christmas?blacklistFlags=nsfw,religious,racist,sexist,explicit&type=single')
     .then(data =>data.json())
     .then(data => {
       // Work with JSON data here
 	const jokeElement = document.getElementById("jokeElement");
 	const p = document.createElement("P");
-     	p.innerHTML = data.value;
+     	p.innerHTML = data.joke;
     	jokeElement.appendChild(p);
     
     
